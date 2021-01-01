9f2a05284530528cf0ecbca522b532d7143cd109
Deepak Somarajan <deepaksrn11@gmail.com>
2021-01-01 23:34:02 +0530

    Update age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -1,4 +1,5 @@
 # Calculates your age based on your Date of Birth
+# This code was developed in 2020, make sure to change it to the current year in line 4
 birth_year = int(input("Enter your birth year : "))
 age = 2020 - birth_year
 print("You are",age,"years old !!")
